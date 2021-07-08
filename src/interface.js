468746177f19fc5604ddf9569691d2cf815d430d
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-07-08 15:47:43 +0100

    added user input for location

--- src/interface.js
+++ src/interface.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   const thermostat = new Thermostat();
+  updateTemperature();
 
   document.querySelector('#temperature-up').addEventListener('click', () => {
     thermostat.up();
@@ -27,20 +28,26 @@ document.addEventListener("DOMContentLoaded", () => {
     thermostat.switchPowerSavingModeOn();
     document.querySelector('#power-saving-status').innerText = 'on';
     updateTemperature();
-  })
+  });
 
   document.querySelector('#powersaving-off').addEventListener('click', () => {
     thermostat.switchPowerSavingModeOff();
     document.querySelector('#power-saving-status').innerText = 'off';
     updateTemperature();
-  })
-
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    document.querySelector('#current-temperature').innerText = data.main.temp;
   });
+
+  document.querySelector('#select-city').addEventListener('submit', (event)=> {
+    event.preventDefault();
+    const city = document.querySelector('#current-city').value;
+    displayWeather(city)
+  });
+
+  const displayWeather = (city) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+  
+    fetch(url)
+    .then(response => response.json())
+    .then(data => document.querySelector('#current-temperature').innerText = data.main.temp)
+  };
 
 });
