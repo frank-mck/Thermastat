e5b88406c04f4e4e5fe977798b05cd78c5a488c0
Frank Mckenna <fmckenna56330@gmail.com>
2021-07-08 14:58:00 +0100

    called a weather api using fetch

--- src/interface.js
+++ src/interface.js
@@ -34,4 +34,13 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#power-saving-status').innerText = 'off';
     updateTemperature();
   })
+
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    document.querySelector('#current-temperature').innerText = data.main.temp;
+  });
+
 });
